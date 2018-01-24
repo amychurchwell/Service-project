dd683143f29efd301da444cd29f40e612c5badd5
Guest User <Guest@Epicodus-3D.local>
2018-01-23 16:24:43 -0800

    fireplace

---
+++ src/app/api-keys.ts
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  export var masterFirebaseConfig = {
+    apiKey: "AIzaSyCQP0PwTjh9ZigdmNNa_SQHVHWCGwCHXm4",
+    authDomain: "amy-zing-project.firebaseapp.com",
+    databaseURL: "https://amy-zing-project.firebaseio.com",
+    projectId: "amy-zing-project",
+    storageBucket: "",
+    messagingSenderId: "911218591674"
+  };
